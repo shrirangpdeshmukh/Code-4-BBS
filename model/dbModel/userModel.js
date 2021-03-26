82596080d3fea09108ca6ecd42f0fc77c2e88193
Shrirang <deshmukhshrirang01@gmail.com>
2021-03-27 00:21:08 +0530

    Added Roll Number

--- model/dbModel/userModel.js
+++ model/dbModel/userModel.js
@@ -33,6 +33,9 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "Not Specified",
     },
+    rollNumber: {
+      type: String,
+    },
 
     admissionYear: {
       type: Number,
